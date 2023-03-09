211649361a4a99453eb59debaf10a2ce02085e50
Stephany <stephanykleen@gmail.com>
2023-03-08 23:09:59 -0800

    Create Meme Generator Header

---
+++ src/components/Header.js
@@ -0,0 +1,8 @@
+export const Header = () =>{
+  return(
+    <div className="header">
+      <img className="header--image" alt="meme-logo" src="../images/Logo.png"/>
+      <p className="header--text">React Course - Project 3</p>
+    </div>
+  )
+}
