98c1e086368463c7600c4c5f639c5fed7b2a9cae
Stephany <stephanykleen@gmail.com>
2023-03-09 19:47:05 -0800

    Create a event listener function within Meme component

--- src/components/Meme.js
+++ src/components/Meme.js
@@ -1,13 +1,20 @@
+import DataMemes from '../Data'
+
 export const Meme = () =>{
+  function GetMemeImg (){
+    const memeArray = DataMemes.data.memes
+    const randomNumber = Math.floor(Math.random()* memeArray.length)
+    const generateMeme = memeArray[randomNumber].url
+    console.log(generateMeme)    
+  }
   return(
     <main>
-      <form className="meme--form">
+      <div className="meme--form">
         <input className="meme--input-left" type="text" placeholder="shut up"/>
         <input className="meme--input-right" type="text" placeholder="take my money"/>
-        
-          <button className="meme--button"> Get a new meme image 🖼️</button>
-        
-      </form>
+        <button onClick={GetMemeImg} className="meme--button"> Get a new meme image 🖼️</button>
+      </div>
+
     </main>
   )
 }
